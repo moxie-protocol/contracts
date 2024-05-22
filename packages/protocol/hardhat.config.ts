ab6483aa42238cbe698821bdd3966b148b6558d9
0xsarvesh <sarvesh.codeworks@gmail.com>
2024-05-22 18:46:31 +0400

    Added solhint

--- packages/protocol/hardhat.config.ts
+++ packages/protocol/hardhat.config.ts
@@ -1,5 +1,6 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-solhint";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
