96fa56d16974aa3636840b881e62c0f39ac41140
Sarvesh Jain <sarvesh.codeworks@gmail.com>
2024-05-24 17:27:25 +0400

    Repo setup (#1)
    * First commit
    
    * Added repo structure
    
    * Added hardhat repos
    
    * Added solhint
    
    * Added project level npm scripts
    
    * Adding  tasks folder

---
+++ packages/protocol/hardhat.config.ts
@@ -0,0 +1,10 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-solhint";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.24",
+};
+
+
+export default config;
