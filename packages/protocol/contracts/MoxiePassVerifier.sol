bbbdd795e122ff7daa2c1e8d2e9857fc14e6809f
Deepesh KN <deepesh@airstack.xyz>
2024-06-06 09:58:35 +0400

    Code review changes

--- packages/protocol/contracts/MoxiePassVerifier.sol
+++ packages/protocol/contracts/MoxiePassVerifier.sol
@@ -1,19 +1,16 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.19;
 
-// @dev The following global imports are suppressed in solhint as there is no name conflicts in the imported names.
-// solhint-disable-next-line no-global-import
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-// solhint-disable-next-line no-global-import
 import "@openzeppelin/contracts/access/Ownable.sol";
-// solhint-disable-next-line no-global-import
 import "./lib/AllowListVerifierHelper.sol";
-// solhint-disable-next-line no-global-import
 import "./interface/IAllowListVerifier.sol";
 
-/// @title Whitelisting Contract with ERC-721 Address Setter
-/// @notice This contract allows the owner to set the address of an ERC-721 contract.
-/// @dev This function is restricted to the contract owner and sets the address for an ERC-721 contract.
+/**
+ * @title Whitelisting Contract with ERC-721 Address Setter
+ * @notice This contract allows the owner to set the address of an ERC-721 contract.
+ * @dev This function is restricted to the contract owner and sets the address for an ERC-721 contract.
+ */
 contract MoxiePassVerifier is Ownable, IAllowListVerifier {
     IERC721 public erc721ContractAddress;
 
@@ -23,7 +20,7 @@ contract MoxiePassVerifier is Ownable, IAllowListVerifier {
     error NotaMoxiePassHolder();
 
     /**
-     * @dev Modifier to make a function callable only when the msg.sender holds moxie padd
+     * @dev Modifier to make a function callable only when the msg.sender holds moxie pass
      */
     modifier onlyMoxiePassHolder() {
         isMoxiePassHolder();
@@ -32,9 +29,13 @@ contract MoxiePassVerifier is Ownable, IAllowListVerifier {
 
     constructor(address _ownerAddress) Ownable(_ownerAddress) {}
 
-    /// @notice Sets the address of the ERC-721 contract.
-    /// @dev This function can only be called by the contract owner. It sets the `erc721ContractAddress` to the specified address. By design, the contract owner can set the erc721ContractAddress to 0 to disable the allow list check.
-    /// @param _erc721ContractAddress The address of the ERC-721 contract to be set.
+    /**
+     * @notice Sets the address of the ERC-721 contract.
+     * @dev This function can only be called by the contract owner.
+     *  It sets the `erc721ContractAddress` to the specified address.
+     *  By design, the contract owner can set the erc721ContractAddress to 0 to disable the allow list check.
+     * @param _erc721ContractAddress The address of the ERC-721 contract to be set.
+     */
     function setErc721ContractAddress(
         address _erc721ContractAddress
     ) public onlyOwner {
@@ -42,7 +43,10 @@ contract MoxiePassVerifier is Ownable, IAllowListVerifier {
     }
 
     /**
-     * @dev Throws if the contract is paused.
+     * @notice Checks if the caller is a Moxie Pass holder (internal function).
+     * @dev This function checks if the caller holds the Moxie Pass NFT.
+     * If the caller does not hold the NFT, the function reverts with the error `NotaMoxiePassHolder`.
+     * If the erc721ContractAddress is not set, the function does not perform the check.
      */
     function isMoxiePassHolder() internal view virtual {
         if (
@@ -53,6 +57,11 @@ contract MoxiePassVerifier is Ownable, IAllowListVerifier {
         }
     }
 
+    /**
+     * @notice Checks if the caller is a Moxie Pass holder.
+     * @param _address The address to check if it is a Moxie Pass holder.
+     * @return A boolean indicating whether the address is a Moxie Pass holder.
+     */
     function isMoxiePassHolder(address _address) external view returns (bool) {
         if (address(erc721ContractAddress) == address(0)) {
             return true;
@@ -60,16 +69,17 @@ contract MoxiePassVerifier is Ownable, IAllowListVerifier {
         return erc721ContractAddress.balanceOf(_address) > 0;
     }
 
-    /// @notice Checks if the specified user is allowed to perform an action in the given auction.
-    /// @dev The function is a view function and does not modify state. It returns a bytes4 selector indicating whether the action is allowed.
-    /// @param user The address of the user whose permission is being checked.
-    /// @param auctionId The ID of the auction in which the action is to be performed.
-    /// @param callData The calldata associated with the action to be performed.
-    /// @return A bytes4 selector indicating whether the user is allowed to perform the action in the auction.
+    /**
+     * @notice Checks if the specified user is allowed to perform an action in the given auction.
+     *  @dev The function is a view function and does not modify state. It returns a bytes4 selector indicating whether the action is allowed.
+     *  @param user The address of the user whose permission is being checked.
+     *  @param auctionId The ID of the auction in which the action is to be performed.
+     *  @param callData The calldata associated with the action to be performed.
+     *  @return A bytes4 selector indicating whether the user is allowed to perform the action in the auction.
+     */
     function isAllowed(
         address user,
         uint256 auctionId,
-        // solhint-disable-next-line no-unused-vars
         bytes calldata callData
     ) external view returns (bytes4) {
         // If the user address is 0, return 0
