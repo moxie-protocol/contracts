4ce7ddd01ab3fc5e92109cd95cf67b31cd88af95
Sharath V <sharathkrml@gmail.com>
2024-06-21 21:50:12 +0530

    changed solidity versions

--- packages/protocol/contracts/MoxiePassVerifier.sol
+++ packages/protocol/contracts/MoxiePassVerifier.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.24;
 
 import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
