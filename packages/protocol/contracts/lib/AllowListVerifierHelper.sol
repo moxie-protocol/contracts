2c7e212ada09a22ea93668e60d5a089e67452270
Gopi Bathala <gopi@airstack.xyz>
2024-06-27 16:04:15 +0400

    sync with dev

--- packages/protocol/contracts/lib/AllowListVerifierHelper.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: LGPL-3.0-or-later
-pragma solidity >=0.6.8;
-
-library AllowListVerifierHelper {
-    /// @dev Value returned by a call to `isAllowed` if the check
-    /// was successful. The value is defined as:
-    /// bytes4(keccak256("isAllowed(address,uint256,bytes)"))
-    bytes4 internal constant MAGICVALUE = 0x19a05a7e;
-}
