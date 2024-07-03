a284137938148827c8f07c54af19e96cc923afe7
0xsarvesh <sarvesh.codeworks@gmail.com>
2024-07-03 09:01:20 -0700

    Added moxie token transfer script

--- packages/protocol/ignition/modules/2_MoxieToken.ts
+++ packages/protocol/ignition/modules/2_MoxieToken.ts
@@ -1,9 +1,13 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+import config from '../config/config.json';
 
+const MOXIE_SUPPLY = '10000000000000000000000000000';
 export default buildModule("MoxieToken", (m) => {
 
   const owner = m.getAccount(1);
   const moxieToken = m.contract("MoxieToken", [], { from: owner });
-  //todo transfer initial mint to multi sig 
+
+  
+  m.call(moxieToken, "transfer", [config.moxieTokenBeneficiary , MOXIE_SUPPLY], {from:owner})
   return { moxieToken };
 });
