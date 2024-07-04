70b18e847f52fdee545fa4315e9575ffccfd6b16
0xsarvesh <sarvesh.codeworks@gmail.com>
2024-07-04 10:40:05 -0700

    Added bid & buy

--- packages/protocol/tasks/Bid.ts
+++ packages/protocol/tasks/Bid.ts
@@ -1,12 +1,25 @@
 import { task } from "hardhat/config";
 import EasyAuctionArtifact from "../test-artifact/easy-auction/artifacts/EasyAuction.json";
-import addresses from '../ignition/deployments/testnet-v2/deployed_addresses.json'
+import { EasyAuction } from "../test-artifact/easy-auction/typechain/EasyAuction";
 
-const VESTING_CONTRACT_ADDRESS = '';
-const AUCTION_ID = 1;
+const VESTING_CONTRACT_ADDRESS = ''
+const AUCTION_ID = 0;
 
 task("bid", "BId in auction", async (taskArgs, hre) => {
 
-    const easyAuction = await hre.ethers.getContractAtFromArtifact(EasyAuctionArtifact, addresses["EasyAuctionContracts#EasyAuction"]);;
+    const buyAmount = '0'; // subject
+    const sellAmount = '0' // moxie
+    const [bidder] = await hre.ethers.getSigners();
 
+    const easyAuction = await hre.ethers.getContractAtFromArtifact(EasyAuctionArtifact, VESTING_CONTRACT_ADDRESS) as unknown as EasyAuction
+
+    const queueStartElement =
+                "0x0000000000000000000000000000000000000000000000000000000000000001";
+    await easyAuction.connect(bidder).placeSellOrders(
+        AUCTION_ID,
+        [buyAmount],
+        [sellAmount],
+        [queueStartElement],
+        '0x'
+    );
 });
--- packages/protocol/tasks/Buy.ts
+++ packages/protocol/tasks/Buy.ts
@@ -1,1 +1,24 @@
 
+import { task } from "hardhat/config";
+import EasyAuctionArtifact from "../test-artifact/easy-auction/artifacts/EasyAuction.json";
+import { EasyAuction } from "../test-artifact/easy-auction/typechain/EasyAuction";
+
+const VESTING_CONTRACT_ADDRESS = ''
+const SUBJECT_ADDRESS = '';
+
+task("buy", "buy from bonding curve ", async (taskArgs, hre) => {
+
+    const buyAmount = '0'; // subject
+    const depositAmount = '';
+
+    const [buyer] = await hre.ethers.getSigners();
+
+    const easyAuction = await hre.ethers.getContractAt('MoxieBondingCurve', VESTING_CONTRACT_ADDRESS);
+
+    await easyAuction.connect(buyer).buyShares(
+        SUBJECT_ADDRESS,
+        depositAmount,
+        buyer.address,
+        0
+    );
+});
