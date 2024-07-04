7e31f7e77ee5200264c9f3ab45cca199591dd630
sarvesh371 <sarveshsingh.03@gmail.com>
2024-07-04 23:01:05 +0530

    added tasks for claim and sell tokens

--- packages/protocol/tasks/Claim.ts
+++ packages/protocol/tasks/Claim.ts
@@ -1,1 +1,36 @@
 
+import { task } from "hardhat/config";
+import EasyAuctionArtifact from "../test-artifact/easy-auction/artifacts/EasyAuction.json";
+import addresses from '../ignition/deployments/testnet-v2/deployed_addresses.json'
+import { BigNumber} from "@ethersproject/bignumber";
+
+const AUCTION_ID = 1;
+
+task("claim", "Claim from auction", async (taskArgs, hre) => {
+
+    const easyAuction = await hre.ethers.getContractAtFromArtifact(EasyAuctionArtifact, addresses["EasyAuctionContracts#EasyAuction"]);;
+
+    await easyAuction.claimFromParticipantOrder(AUCTION_ID, [
+        encodeOrder({
+            sellAmount: BigNumber.from("12"),
+            buyAmount: BigNumber.from("700"),
+            userId: BigNumber.from(2),
+          }),
+    ]);
+
+});
+
+export function encodeOrder(order: Order): string {
+    return (
+      "0x" +
+      order.userId.toHexString().slice(2).padStart(16, "0") +
+      order.buyAmount.toHexString().slice(2).padStart(24, "0") +
+      order.sellAmount.toHexString().slice(2).padStart(24, "0")
+    );
+}
+
+export interface Order {
+    sellAmount: BigNumber;
+    buyAmount: BigNumber;
+    userId: BigNumber;
+}
--- packages/protocol/tasks/Sell.ts
+++ packages/protocol/tasks/Sell.ts
@@ -1,1 +1,21 @@
+import { task } from "hardhat/config";
 
+const MOXIE_BONDING_CURVE_ADDRESS = '0xc3216954C25d18a366A67A86c64C7db9c8D62e45';
+const SUBJECT_ADDRESS = ''
+
+task("sell", "Sell tokens", async (taskArgs, hre) => {
+
+    const moxieBondingCurve = await hre.ethers.getContractAt("MoxieBondingCurve", MOXIE_BONDING_CURVE_ADDRESS);
+
+    // Get signer using a address
+    const seller = await hre.ethers.getSigner("0x00d620DEF6Ccb76C92dBFC87bC2bebaB7637eC53");
+    moxieBondingCurve
+        .connect(seller)
+        .sellShares(
+        SUBJECT_ADDRESS,
+        'totalSellAmountSeller1',
+        seller.address,
+        0,
+    );
+
+});
