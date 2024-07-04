49b447edd69426257462bd4dc6a10a35ec991d68
sarvesh371 <sarveshsingh.03@gmail.com>
2024-07-04 23:18:24 +0530

    added vesting contract address in claim and sell tasks

--- packages/protocol/tasks/Claim.ts
+++ packages/protocol/tasks/Claim.ts
@@ -1,14 +1,14 @@
 
 import { task } from "hardhat/config";
 import EasyAuctionArtifact from "../test-artifact/easy-auction/artifacts/EasyAuction.json";
-import addresses from '../ignition/deployments/testnet-v2/deployed_addresses.json'
 import { BigNumber} from "@ethersproject/bignumber";
 
 const AUCTION_ID = 1;
+const VESTING_CONTRACT_ADDRESS = ''
 
 task("claim", "Claim from auction", async (taskArgs, hre) => {
 
-    const easyAuction = await hre.ethers.getContractAtFromArtifact(EasyAuctionArtifact, addresses["EasyAuctionContracts#EasyAuction"]);;
+    const easyAuction = await hre.ethers.getContractAtFromArtifact(EasyAuctionArtifact, VESTING_CONTRACT_ADDRESS);;
 
     await easyAuction.claimFromParticipantOrder(AUCTION_ID, [
         encodeOrder({
--- packages/protocol/tasks/Sell.ts
+++ packages/protocol/tasks/Sell.ts
@@ -1,11 +1,11 @@
 import { task } from "hardhat/config";
 
-const MOXIE_BONDING_CURVE_ADDRESS = '0xc3216954C25d18a366A67A86c64C7db9c8D62e45';
+const VESTING_CONTRACT_ADDRESS = ''
 const SUBJECT_ADDRESS = ''
 
 task("sell", "Sell tokens", async (taskArgs, hre) => {
 
-    const moxieBondingCurve = await hre.ethers.getContractAt("MoxieBondingCurve", MOXIE_BONDING_CURVE_ADDRESS);
+    const moxieBondingCurve = await hre.ethers.getContractAt("MoxieBondingCurve", VESTING_CONTRACT_ADDRESS);
 
     // Get signer using a address
     const seller = await hre.ethers.getSigner("0x00d620DEF6Ccb76C92dBFC87bC2bebaB7637eC53");
